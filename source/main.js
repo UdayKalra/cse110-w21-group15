b155dcd967e11f2b3b75a5437356b4a529b95049
Dylan Palmer <dcpalmer@ucsd.edu>
2021-02-23 21:23:36 -0800

    combining nhat timer work and dylan tasklist work

--- source/main.js
+++ source/main.js
@@ -1,119 +1,134 @@
-/* imports */
-// import { editSettings } from './settings.js'
-// import { timer } from './timer.js'
-function docID(id){
-  return document.getElementById(id);
+
+// Initialize all global variables.
+const pomoSession = {
+  count: 0, /* 4 to a set */
+  sets: 0, /* counts how many full pomo sets completed */
+  state: 'work', /* can be work, shortBreak, or longBreak */
+  pomoLen: 0.5, /* these are all set low for testing */
+  shortBreakLen: 0.2,
+  longBreakLen: 0.2,
+  firstStart: true
 }
 
-function main () {
-  /* this object holds all the default settings we used,
-     some of which will be by the settings menu */
-  const pomoSession = {
-    count: 0, /* 4 to a set */
-    sets: 0, /* counts how many full pomo sets completed */
-    state: 0, /* can be 0= work, 1=shortBreak, or 2=longBreak */
-    pomoLen: 0.1, /* these are all set low for testing */
-    shortBreakLen: 0.1,  // in minutes
-    longBreakLen: 15
-  }
+// pomoSession.pomoLen = 0.5
 
-  const startButton = document.getElementById('play')
+let timerLen
+let timerRef
+let mins
+let seconds
 
-  /* this will later be replaced by a more general purpose settings listener */
-  document.getElementById('pomo-time').addEventListener('change', function (event) { runSettings(event) })
+// Add all EventListener when the DOM Loaded
+document.addEventListener('DOMContentLoaded', function (event) {
+  document.getElementById('play').addEventListener('click', startSession)
+  document.getElementById('stop').addEventListener('click', stopSession)
+})
 
-  function runSettings (event) {
-    event.preventDefault();
-    session.pomoLen = document.getElementById('pomo-time').value
-    document.getElementById('time').innerHTML = session.pomoLen + ':00'
-  }
+function startSession () {
+  // Change Start button to Stop button
+  document.getElementById('play').style.display = 'none'
+  document.getElementById('stop').style.display = 'block'
 
-  startButton.addEventListener('click', function (event) { runSession(event) })
-
-  /* when the start button is pressed this function runs
-     it's purpose is to reset the state to working and
-     call the timer function */
-  function runSession (event) {
-    /* TODO: in here we should also prevent start from being clickable
-       until the end of the break times */
-    event.preventDefault()
-    // playAudio() /*if we choose to */
-
-    if (pomoSession.count === 4) {
-      pomoSession.sets++
-      pomoSession.count = 0
-      document.getElementById('pomo-count').innerHTML = 'pomos: 0'
-      document.getElementById('pomo-sets').innerHTML = 'pomo sets: ' + pomoSession.sets
-    }
-    pomoSession.state = 0;
-    // document.getElementById('pomo-state').innerHTML = 'work'
-    timer(pomoSession)
-  }
+  // Start the timer
+  runTimer()
 }
 
-main()
+function stopSession () {
+  // Reset the pomoSession variable
+  pomoSession.state = 'work'
+  pomoSession.count = 0
+  pomoSession.sets = 1
+  pomoSession.firstStart = true
 
-function timer (pomoSession) {
-  let timerLen = pomoSession.pomoLen * 60 * 1000 /* pomoLen minutes -> miliseconds */
-  timerLen -= 1000
-  updateTime()
+  // Change Stop button to Start button
+  document.getElementById('play').style.display = 'block'
+  document.getElementById('stop').style.display = 'none'
+  // Display the timer in Pomotime
+  timerLen = updateTimerLen()
+  displayMinSecond()
+  // Stop the timer
+  clearInterval(timerRef)
+}
 
-  let timerRef = setInterval(function () { updateTime() }, 1000)
-
-  /* this function uses the timerLen to find our seconds and
-     minutes to display on the clock. then when the timer runs out
-     it determines what state to move to based on current state */
-  function updateTime () {
-    let mins = Math.floor((timerLen / 1000) / 60)
-    let seconds = (timerLen / 1000) % 60
-
-    /* these if statements tell us if we need to pad our mins/seconds
-       numbers with an extra 0 */
-    if (mins < 10) {
-      mins = '0' + mins
-    }
-
-    if (seconds < 10) {
-      seconds = '0' + seconds
-    }
-
-    /* update the time on the doc */
-    document.getElementById('time').innerHTML = mins + ':' + seconds
-    /* if timer hits 0 we make some changes */
-    if (timerLen <= 0) {
-      // debugger
-      clearInterval(timerRef)
-      stateChange()
-      return
-    }
+function runTimer () {
+  timerLen = updateTimerLen()
+  console.log(timerLen)
+  // Special case for first time start a work state, we need to offet a delay when clicking start button
+  if (pomoSession.firstStart === true) {
     timerLen -= 1000
+    pomoSession.firstStart = false
   }
+  timerRef = setInterval(updateTimer, 1000)
+}
 
-  /* this function does the actual changes to the document and our
-     session object. it's a bit hefty right now */
-  function stateChange () {
-    if (pomoSession.state === 0) {
-      pomoSession.count++
-      if (pomoSession.count === 4) {
-        pomoSession.state = 2
-        // document.getElementById('pomo-state').innerHTML = 'longBreak'
-        // document.getElementById('pomo-count').innerHTML = 'pomos: ' + pomoSession.count
-        // document.getElementById('pomo-sets').innerHTML = 'pomo sets: ' + pomoSession.sets
-        timerLen = pomoSession.longBreakLen * 60 * 1000
-        updateTime()
-        timerRef = setInterval(function () { updateTime() }, 1000)
-      } else {
-        pomoSession.state = 1
-        // document.getElementById('pomo-state').innerHTML = 'shortBreak'
-        // document.getElementById('pomo-count').innerHTML = 'pomos: ' + pomoSession.count
-        // document.getElementById('pomo-sets').innerHTML = 'pomo sets: ' + pomoSession.sets
-        timerLen = pomoSession.shortBreakLen * 60 * 1000
-        updateTime()
-        timerRef = setInterval(function () { updateTime() }, 1000)
-      }
-    } else {
-      timerLen = pomoSession.pomoLen * 60 * 1000
-      updateTime()
-    }
+function updateTimerLen () {
+  let length
+  // Set the timer length based on its state
+  switch (pomoSession.state) {
+    case 'work':
+      length = pomoSession.pomoLen
+      break
+    case 'shortBreak':
+      length = pomoSession.shortBreakLen
+      break
+    case 'longBreak':
+      length = pomoSession.longBreakLen
+      break
   }
+  return length * 60 * 1000 /* pomoLen in miliseconds */
+}
+
+function displayMinSecond () {
+  console.log(timerLen)
+  mins = Math.floor((timerLen / 1000) / 60)
+  seconds = (timerLen / 1000) % 60
+  if (mins < 10) {
+    mins = '0' + mins
+  }
+  if (seconds < 10) {
+    seconds = '0' + seconds
+  }
+  document.getElementById('time').innerHTML = mins + ':' + seconds
+}
+function updateTimer () {
+  if (timerLen <= 0) {
+    clearInterval(timerRef)
+    stateChange()
+  }
+  displayMinSecond()
+  timerLen -= 1000
+}
+
+/* this function does the actual changes to the document and our
+   session object. it's a bit hefty right now */
+function stateChange () {
+  switch (pomoSession.state) {
+    case 'work':
+      if (pomoSession.count === 4) {
+        pomoSession.state = 'longBreak'
+      } else {
+        pomoSession.state = 'shortBreak'
+      }
+      runTimer()
+      break
+    case 'shortBreak':
+      pomoSession.state = 'work'
+      pomoSession.firstStart = true
+      pomoSession.count++
+      timerLen = updateTimerLen()
+      // Change Stop button to Start button
+      document.getElementById('play').style.display = 'block'
+      document.getElementById('stop').style.display = 'none'
+      break
+    case 'longBreak':
+      pomoSession.state = 'work'
+      pomoSession.firstStart = true
+      pomoSession.count = 0
+      pomoSession.set++
+      updateTimerLen()
+      // Change Stop button to Start button
+      document.getElementById('play').style.display = 'block'
+      document.getElementById('stop').style.display = 'none'
+      break
+  }
+  displayMinSecond()
 }
--- source/tasklist.js
+++ source/tasklist.js
@@ -1,170 +1,202 @@
 /* script for modifying the task list via drag-and-drop
  * this file may later absorb all of the task list functionality */
 
-document.getElementById('list-entry').addEventListener('submit', function (event) { addToList(event) })
+/* TODO:
+   create event listeners and functions for each of the buttons here
+   modify delfromList to work with new modifications
+*/
+function tasklist () {
+  const masterList = []
 
-const list = document.getElementById('task-queue')
-const listItems = []
-const delBtns = []
-let curr
-const dropZones = [document.getElementById('drop-zone')]
+  document.getElementById('add-task').addEventListener('click', addToList)
 
-/* this function adds bullet points and drag spaces to our list
-   there is a cleaner way to do this, will update next sprint :)
-   involves just adding to array then redrawing list, like in the
-   functions to remove or rearrange */
-function addToList (event) {
-  event.preventDefault()
-  const newItem = document.getElementById('task')
-  const li = document.createElement('li')
+  const list = document.getElementById('tasks')
+  const listItems = []
+  const delBtns = []
+  const dropZones = []
+  let curr
 
-  /* here we set the id of new bullet point to 'task' followed its position in
-     the list to give them each a unique id */
-  li.setAttribute('draggable', true)
-  const delBtn = document.createElement('BUTTON')
-  delBtn.innerHTML = 'X'
-  delBtn.setAttribute('id', 'del-btn')
-  const dropZone = document.createElement('DIV')
-  dropZone.setAttribute('id', 'drop-zone')
-  li.appendChild(document.createTextNode(newItem.value))
-  li.appendChild(delBtn)
-  listItems.push(li)
-  list.appendChild(li)
-  list.appendChild(dropZone)
+  function buildNewTask () {
+    const taskInput = document.getElementById('pomo-task')
+    const newTask = document.createElement('div')
+    newTask.setAttribute('class', 'task-object')
 
-  /* both of the below loops that set event listeners are done pretty
-     primitively and create redundant listeners, I'll find a better way of
-     doing it */
+    /* fill task object with it's buttons and text elements */
+    const moveButton = document.createElement('button')
+    moveButton.setAttribute('class', 'move-task-button')
+    moveButton.innerHTML = '<img src="assets/moveTask.svg" alt="move task" id="move-task-icon">'
+    newTask.appendChild(moveButton)
 
-  /* this lets us set event listeners for all the buttons we create
-     it doesn't seem to work if we set 1 listener for all buttons go figure */
-  delBtns.push(delBtn)
-  for (let i = 0; i < delBtns.length; i++) {
-    delBtns[i].addEventListener('click', delFromList)
+    const delButton = document.createElement('button')
+    delButton.setAttribute('class', 'delete-task-button')
+    delButton.innerHTML = '<img src="assets/minusSign.svg" alt="delete task" id="delete-task-icon">'
+    newTask.appendChild(delButton)
+
+    const pomoNum = document.createElement('p')
+    pomoNum.setAttribute('class', 'task-pomo-num')
+    pomoNum.innerHTML = '0'
+    newTask.appendChild(pomoNum)
+
+    const taskText = document.createElement('p')
+    taskText.setAttribute('class', 'task-text')
+    taskText.innerHTML = taskInput.value
+    newTask.appendChild(taskText)
+
+    const compButton = document.createElement('button')
+    compButton.setAttribute('class', 'complete-task-button')
+    compButton.innerHTML = '<img src="assets/checkTask.svg" alt="complete task" id="complete-task-icon">'
+    newTask.appendChild(compButton)
+
+    taskInput.value = ''
+
+    return newTask
   }
 
-  /* this sets listeners for our drop areas to let us drop tasks into other spots */
-  /* dragenter: when mouse enters boundary of drop area
-     dragleave: when mouse leaves boundary of drop area
-     dragover:  when element is being dragged over another
-     drop:      when element is dropped */
-  dropZones.push(dropZone)
-  for (let i = 0; i < dropZones.length; i++) {
-    dropZones[i].addEventListener('dragenter', function () { dragIn(this) })
-    dropZones[i].addEventListener('dragleave', function () { dragOut(this) })
-    dropZones[i].addEventListener('dragover', function (event) { dragOn(event) })
-    dropZones[i].addEventListener('drop', function (event) { dropMove(event, this) })
+  /* this function adds bullet points and drag spaces to our list
+     there is a cleaner way to do this, will update next sprint :)
+     involves just adding to array then redrawing list, like in the
+     functions to remove or rearrange */
+  function addToList () {
+    const newTask = buildNewTask()
+    const tmptask = {
+      taskBody: newTask,
+      time: 0
+    }
+    masterList.push(tmptask)
+
+    const dropZone = document.createElement('div')
+    dropZone.setAttribute('id', 'drop-zone')
+
+    /* this sets listeners for our drop areas to let us drop tasks into other spots */
+    /* dragenter: when mouse enters boundary of drop area
+       dragleave: when mouse leaves boundary of drop area
+       dragover:  when element is being dragged over another
+       drop:      when element is dropped */
+    dropZones.push(dropZone)
+    for (let i = 0; i < dropZones.length; i++) {
+      dropZones[i].addEventListener('dragenter', function () { dragIn(this) })
+      dropZones[i].addEventListener('dragleave', function () { dragOut(this) })
+      dropZones[i].addEventListener('dragover', function (event) { dragOn(event) })
+      dropZones[i].addEventListener('drop', function (event) { dropMove(event, this) })
+    }
+
+    /* these are listeners for when we drag a task item */
+    /* dragstart: mouse is held and moved
+       dragend:   when drag stops (seems similar to drop but follows the element
+       being dragged instead of the drop zone) */
+    newTask.addEventListener('dragstart', function () { dragTask(this) })
+    newTask.addEventListener('dragend', function () { dropEnd() })
+
+    redrawList()
   }
 
-  /* these are listeners for when we drag a task item */
-  /* dragstart: mouse is held and moved
-     dragend:   when drag stops (seems similar to drop but follows the element
-     being dragged instead of the drop zone) */
-  li.addEventListener('dragstart', function () { dragTask(this) })
-  li.addEventListener('dragend', function () { dropEnd() })
-  newItem.value = ''
-}
+  /* removes an item from our task list */
+  // function delFromList () {
+  //   /* get rid of the button and bullet from our arrays before deleting it */
+  //   listItems.splice(delBtns.indexOf(this), 1)
+  //   delBtns.splice(delBtns.indexOf(this), 1)
 
-/* removes an item from our task list */
-function delFromList () {
-  /* get rid of the button and bullet from our arrays before deleting it */
-  listItems.splice(delBtns.indexOf(this), 1)
-  delBtns.splice(delBtns.indexOf(this), 1)
+  //   /* also get rid of the drop zone below the element being deleted */
+  //   dropZones.splice(delBtns.indexOf(this), 1)
 
-  /* also get rid of the drop zone below the element being deleted */
-  dropZones.splice(delBtns.indexOf(this), 1)
+  //   redrawList()
+  // }
 
-  redrawList()
-}
+  /* highlights valid drop zones when the dragged item moves over them */
+  function dragIn (zone) {
+    zone.setAttribute('class', 'active')
+  }
 
-/* highlights valid drop zones when the dragged item moves over them */
-function dragIn (zone) {
-  zone.setAttribute('class', 'active')
-}
+  /* removes highlight on drop zones when dragged item leaves */
+  function dragOut (zone) {
+    zone.setAttribute('class', '')
+  }
 
-/* removes highlight on drop zones when dragged item leaves */
-function dragOut (zone) {
-  zone.setAttribute('class', '')
-}
+  /* dragover default needs to be overridden for proper dropping */
+  function dragOn (event) {
+    event.preventDefault()
+  }
 
-/* dragover default needs to be overridden for proper dropping */
-function dragOn (event) {
-  event.preventDefault()
-}
+  /* this just helps us keep track of which bullet point is being moved */
+  function dragTask (item) {
+    curr = item
+  }
 
-/* this just helps us keep track of which bullet point is being moved */
-function dragTask (item) {
-  curr = item
-}
+  /* rearranges the list when an item is dropped */
+  function dropMove (event, item) {
+    /* NOTE: there is definitely a more clever way of doing this
+       involving the indexOf function
+       these weird for loops basically find out where the item
+       and dropzone is in their respective arrays */
+    let bulletIndex
+    let dropIndex
+    event.preventDefault()
+    for (let i = 0; i < listItems.length; i++) {
+      if (curr === listItems[i]) {
+        bulletIndex = i
+      }
+    }
 
-/* rearranges the list when an item is dropped */
-function dropMove (event, item) {
-  /* NOTE: there is definitely a more clever way of doing this
-     involving the indexOf function
-     these weird for loops basically find out where the item
-     and dropzone is in their respective arrays */
-  let bulletIndex
-  let dropIndex
-  event.preventDefault()
-  for (let i = 0; i < listItems.length; i++) {
-    if (curr === listItems[i]) {
-      bulletIndex = i
+    for (let i = 0; i < dropZones.length; i++) {
+      if (item === dropZones[i]) {
+        dropIndex = i
+      }
+    }
+
+    /* then we rearrange their array positions and re-draw the task list */
+    movePos(bulletIndex, dropIndex)
+    redrawList()
+
+    dropEnd()
+  }
+
+  /* this is for manually removing the highlights, similar to dragOut function
+     but when we rearrange it only fires a dragend event and not
+     a dragleave so the highlight stays haha */
+  function dropEnd () {
+    for (const i of dropZones) {
+      i.setAttribute('class', '')
     }
   }
 
-  for (let i = 0; i < dropZones.length; i++) {
-    if (item === dropZones[i]) {
-      dropIndex = i
+  /* take our bullet point and drop zone and use them to rewrite the array */
+  function movePos (bullet, drop) {
+    const tmpB = listItems[bullet]
+    const tmpD = delBtns[bullet]
+
+    /* delete old bullet then put it in new spot in array */
+    listItems.splice(bullet, 1)
+    delBtns.splice(bullet, 1)
+    if (drop === 0) {
+      listItems.splice(drop, 0, tmpB)
+      delBtns.splice(drop, 0, tmpD)
+    } else {
+      listItems.splice(drop - 1, 0, tmpB)
+      delBtns.splice(drop - 1, 0, tmpD)
     }
   }
 
-  /* then we rearrange their array positions and re-draw the task list */
-  movePos(bulletIndex, dropIndex)
-  redrawList()
+  /* this deletes all of the children of the task list and repopulates them
+     using the arrays that represent their contents */
+  function redrawList () {
+    while (list.firstChild) {
+      list.removeChild(list.firstChild)
+    }
 
-  dropEnd()
-}
+    /* this is the first dropZone, then every bullet point also has an
+       associated dropZone below it so we have coverage above and below
+       every bullet */
+    // list.appendChild(dropZones[0])
 
-/* this is for manually removing the highlights, similar to dragOut function
-   but when we rearrange it only fires a dragend event and not
-   a dragleave so the highlight stays haha */
-function dropEnd () {
-  for (const i of dropZones) {
-    i.setAttribute('class', '')
+    /* for (let i = 1; i < dropZones.length; i++) {
+       list.appendChild(listItems[i - 1])
+       list.appendChild(dropZones[i])
+       } */
+
+    for (let i = 0; i < masterList.length; i++) {
+      list.appendChild(masterList[i].taskBody)
+    }
   }
 }
 
-/* take our bullet point and drop zone and use them to rewrite the array */
-function movePos (bullet, drop) {
-  const tmpB = listItems[bullet]
-  const tmpD = delBtns[bullet]
-
-  /* delete old bullet then put it in new spot in array */
-  listItems.splice(bullet, 1)
-  delBtns.splice(bullet, 1)
-  if (drop === 0) {
-    listItems.splice(drop, 0, tmpB)
-    delBtns.splice(drop, 0, tmpD)
-  } else {
-    listItems.splice(drop - 1, 0, tmpB)
-    delBtns.splice(drop - 1, 0, tmpD)
-  }
-}
-
-/* this deletes all of the children of the task list and repopulates them
-   using the arrays that represent their contents */
-function redrawList () {
-  while (list.firstChild) {
-    list.removeChild(list.firstChild)
-  }
-
-  /* this is the first dropZone, then every bullet point also has an
-     associated dropZone below it so we have coverage above and below
-     every bullet */
-  list.appendChild(dropZones[0])
-
-  for (let i = 1; i < dropZones.length; i++) {
-    list.appendChild(listItems[i - 1])
-    list.appendChild(dropZones[i])
-  }
-}
+tasklist()
